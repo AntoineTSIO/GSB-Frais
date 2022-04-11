1a086c41cd37a9fd8f922677c1f2bd99ae36fdd5
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-04-11 17:02:08 +0200

    Correction UC4 - Valider fiche de frais + Implémentation complète UC5 - Suivre le paiement fiche de frais

--- src/Controller/ComptableController.php
+++ src/Controller/ComptableController.php
@@ -101,9 +101,41 @@ class ComptableController extends AbstractController
 
         $requeteModifFraisHorsForfait = $connexion->query("UPDATE LigneFraisHorsForfait SET libelle ='".$libelle_FraisHorsForfait_invalide."', mois ='".$moisModifFraisHorsForfait."' WHERE id ='".$id_FraisHorsForfait_invalide."'");
         $requeteModifFraisHorsForfait->execute();
-        $requeteModifDateModif = $connexion->query("UPDATE FicheFrais SET dateModif ='".$dateActuelle."'");
+        $requeteModifDateModif = $connexion->query("UPDATE FicheFrais SET dateModif ='".$dateActuelle."'where mois='".$moisModifFraisHorsForfait."' AND idVisiteur = '".$idVisiteur_FraisHorsForfait_invalide."'");
         $requeteModifDateModif->execute();
 
+
+        //Report d'un frais hors forfait
+        $id_FraisHorsForfait_report = $request->get('id_FraisHorsForfait_report');
+        $libelle_FraisHorsForfait_report = $request->get('libelle_FraisHorsForfait_report');
+        $mois_FraisHorsForfait_report = $request->get('mois_FraisHorsForfait_report');
+        $verif_FraisHorsForfait_report = $request->get('verif_FraisHorsForfait_report');
+        $idVisiteur_FraisHorsForfait_report = $request->get('idVisiteur_FraisHorsForfait_report');
+
+        $anneeActuelle = date('Y');
+        $dateActuelle = date("y-m-d");
+        if($mois_FraisHorsForfait_report == 12){
+            $moisReportFraisHorsForfait = '01' . $anneeActuelle+1;
+        }else if($mois_FraisHorsForfait_report+1 < 10){
+            $moisReportFraisHorsForfait = "0".$mois_FraisHorsForfait_report+1 . $anneeActuelle;
+        }else{
+            $moisReportFraisHorsForfait = $mois_FraisHorsForfait_report+1 . $anneeActuelle;
+        }
+
+        if(isset($verif_FraisHorsForfait_report)){
+            $requeteVerificationFicheFrais = $connexion->query("SELECT * FROM FicheFrais where mois='".$moisReportFraisHorsForfait."' AND idVisiteur = '".$idVisiteur_FraisHorsForfait_report."'");
+            $verificationFicheFrais = $requeteVerificationFicheFrais->fetch();
+            if(empty($verificationFicheFrais[0])){ //Si aucune valeur n'est retournée, une nouvelle fiche de frais doit être créée pour le mois suivant
+                $requeteCreationFicheFrais = "INSERT INTO FicheFrais(`idVisiteur`,`mois`,`dateModif`) VALUES('" . $idVisiteur_FraisHorsForfait_report . "','" . $moisReportFraisHorsForfait . "', '" . $dateActuelle . "')";
+                $resCreationFicheFrais = $connexion->prepare($requeteCreationFicheFrais);
+                $resCreationFicheFrais->execute();
+            }
+        }
+
+        $requeteReportFraisHorsForfait = $connexion->query("UPDATE LigneFraisHorsForfait SET libelle ='".$libelle_FraisHorsForfait_report."', mois ='".$moisReportFraisHorsForfait."' WHERE id ='".$id_FraisHorsForfait_report."'");
+        $requeteReportFraisHorsForfait->execute();
+        $requeteReportDateModif = $connexion->query("UPDATE FicheFrais SET dateModif ='".$dateActuelle."'where mois='".$moisReportFraisHorsForfait."' AND idVisiteur = '".$idVisiteur_FraisHorsForfait_report."'");
+        $requeteReportDateModif->execute();
 
 
         //Invalidation d'un frais hors forfait
@@ -127,6 +159,70 @@ class ComptableController extends AbstractController
             'listeFraisHorsForfait' => $listeFraisHorsForfait,
             'fraisForfaitValidés' => $fraisForfait,
             'fraisHorsForfaitValidés' => $fraisHorsForfait
+        ]);
+    }
+
+
+
+    public function suivreUneFiche(Request $request):Response
+    {
+        try{
+            $dbName = 'gsbFrais';
+            $host = 'localhost';
+            $utilisateur = 'gsb';
+            $motDePasse = 'azerty';
+            $port = '3306';
+            $dns = 'mysql:host='.$host.';dbname='.$dbName.';port='.$port;
+            $connexion = new PDO( $dns, $utilisateur, $motDePasse);
+        } catch (Exception $e) {
+            echo "connection impossible : " . $e;
+            die();
+        }
+
+
+
+
+        //Détail frais forfait et frais hors forfait
+        $moisDetailFraisValides = $request->get('mois');
+        $idDetailVisiteurFraisValides = $request->get('idVisiteur');
+        $idEtat = $request->get('idEtat');
+        $requeteFraisForfait = $connexion->query("SELECT * FROM LigneFraisForfait AS LFF INNER JOIN FraisForfait AS FF ON FF.id = LFF.idFraisForfait WHERE LFF.idVisiteur ='".$idDetailVisiteurFraisValides."' AND LFF.mois ='".$moisDetailFraisValides."'");
+        $requeteFraisHorsForfait = $connexion->query("SELECT * FROM LigneFraisHorsForfait WHERE idVisiteur ='".$idDetailVisiteurFraisValides."' AND mois ='".$moisDetailFraisValides."'");
+        $listeFraisForfait = $requeteFraisForfait->fetchAll();
+        $listeFraisHorsForfait = $requeteFraisHorsForfait->fetchAll();
+
+
+        //Mise en paiement de la fiche de frais
+        $mp_idVisiteur = $request->get('mp_idVisiteur');
+        $mp_mois = $request->get('mp_mois');
+        $requeteMiseEnPaiementFicheFrais = $connexion->query("UPDATE FicheFrais SET idEtat = 'MP' WHERE idVisiteur ='".$mp_idVisiteur."' AND mois = '".$mp_mois."'");
+        $requeteMiseEnPaiementFicheFrais->execute();
+
+        //Remboursement de la fiche de frais
+        $rb_idVisiteur = $request->get('rb_idVisiteur');
+        $rb_mois = $request->get('rb_mois');
+        $varRemboursement = $request->get('rb_varRemboursement');
+        $requeteRemboursementFicheFrais = $connexion->query("UPDATE FicheFrais SET idEtat = 'RB' WHERE idVisiteur ='".$rb_idVisiteur."' AND mois = '".$rb_mois."'");
+        $requeteRemboursementFicheFrais->execute();
+
+
+        //Annuler la ise en paiement de la fiche de frais
+        $va_idVisiteur = $request->get('va_idVisiteur');
+        $va_mois = $request->get('va_mois');
+        $requeteAnnulerMiseEnPaiementFicheFrais = $connexion->query("UPDATE FicheFrais SET idEtat = 'VA' WHERE idVisiteur ='".$va_idVisiteur."' AND mois = '".$va_mois."'");
+        $requeteAnnulerMiseEnPaiementFicheFrais->execute();
+
+
+        //Liste fiches de frais validées
+        $requeteFichesFraisValidees = $connexion->query("SELECT * FROM FicheFrais INNER JOIN Etat ON FicheFrais.idEtat = Etat.id WHERE FicheFrais.idEtat = 'VA' or FicheFrais.idEtat ='MP'");
+        $listeFichesFraisValidees = $requeteFichesFraisValidees -> fetchAll();
+
+        return $this->render('comptable/suivrePaiementFicheFrais.html.twig', [
+            'listeFichesFraisValidees' => $listeFichesFraisValidees,
+            'idEtat' => $idEtat,
+            'listeFraisForfait' => $listeFraisForfait,
+            'listeFraisHorsForfait' => $listeFraisHorsForfait,
+            'varRemboursement' => $varRemboursement
         ]);
     }
 }
